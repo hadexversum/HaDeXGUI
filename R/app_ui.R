c01acd9111511745bcd09fef770bae240206285a
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-15 13:21:16 +0200

    add start tab

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,8 +9,24 @@ app_ui <- function(request) {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
+    h1("Welcome to HaDeX GUI!"),
     fluidPage(
-      h1("HaDeXGUI")
+      fluidRow(
+        column(
+          width = 5,
+          h6("Thank you for using our tool."),
+          h6("Questions/feature requests/commercial applications: hadex@ibb.waw.pl")
+        ),
+        column(
+          width = 5,
+          span("This is the development version of the application and may not be working correctly yet. The official version will be available soon. ", style="color:red")
+        )
+      )
+    ),
+    includeMarkdown(app_sys("app/man/about.md")),
+    img(
+      id = "HaDeX-funding-icons",
+      src = "www/funding_icons.png"
     )
   )
 }
