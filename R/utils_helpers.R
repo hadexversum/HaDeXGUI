23362b486b3d85a80dbfe5394110c0bcb0b981d7
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-20 11:05:19 +0200

    add elements and helpers

---
+++ R/utils_helpers.R
@@ -0,0 +1,29 @@
+#' helpers
+#'
+#' @description A utils function
+#'
+#' @return The return value, if any, from executing the utility.
+#'
+#' @noRd
+plotOutput_h <- function(outputId, ...)
+  helper(withHaDeXSpinner(plotOutput(outputId = outputId, ...)),
+         content = outputId, type = "markdown", buttonLabel = "Okay", easyClose = TRUE,
+         icon = "far fa-question-circle", colour = "#856C9D")
+
+## "DT::dataTableOutput"
+dataTableOutput_h <- function(outputId, ...)
+  helper(getFromNamespace("dataTableOutput", ns = "DT")(outputId = outputId, ...), content = outputId,
+         type = "markdown", buttonLabel = "okay", easyClose = TRUE,
+         icon = "far fa-question-circle", colour = "#856C9D")
+
+func_vec <- c("selectInput", "textInput", "checkboxInput", "numericInput", "radioButtons", "checkboxGroupInput")
+func_list <- setNames(lapply(func_vec, function(ith_fun)
+  tmp_name <- function(inputId, ...) {
+    helper(getFromNamespace(ith_fun, ns = "shiny")(inputId = inputId, ...),  content = inputId,
+           type = "markdown", buttonLabel = "Okay", easyClose = TRUE,
+           icon = "far fa-question-circle", colour = "#856C9D")
+  }), func_vec)
+
+for(ith_fun_id in 1L:length(func_list)) {
+  assign(x = paste0(names(func_list)[ith_fun_id], "_h"), value = func_list[[ith_fun_id]])
+}
