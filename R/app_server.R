108905d67eab31331dc9b1f1ddccec477f67c4ea
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-20 10:47:25 +0200

    add icecream debugging

--- R/app_server.R
+++ R/app_server.R
@@ -3,7 +3,14 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom icecream ic_disable ic_enable
 #' @noRd
 app_server <- function(input, output, session) {
+  if (getOption("golem.app.prod"))
+    ic_disable()
+  else {
+    options(icecream.always.include.context = TRUE)
+    ic_enable()
+  }
   mod_source_reading_server("source_reading")
 }
