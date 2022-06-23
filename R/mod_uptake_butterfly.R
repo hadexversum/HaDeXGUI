8335b468b8eac5158bf5ce3a820ec3a4f41f1884
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-23 11:18:41 +0200

    template for butterfly plot tab

---
+++ R/mod_uptake_butterfly.R
@@ -0,0 +1,31 @@
+#' uptake_butterfly UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_uptake_butterfly_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' uptake_butterfly Server Functions
+#'
+#' @noRd 
+mod_uptake_butterfly_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+ 
+  })
+}
+    
+## To be copied in the UI
+# mod_uptake_butterfly_ui("uptake_butterfly_1")
+    
+## To be copied in the server
+# mod_uptake_butterfly_server("uptake_butterfly_1")
