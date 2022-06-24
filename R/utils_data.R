742611e9f10a11f7b5631cbfaf55d716060c0e11
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-24 12:01:16 +0200

    hadex dt format

--- R/utils_data.R
+++ R/utils_data.R
@@ -3,3 +3,21 @@ htmlize_data <- function(dataset_name) {
   data("file_req", package = "HaDeXGUI", envir = environment())
   HTML(paste(capture.output(print(xtable(file_req), type = "html"))))
 }
+
+
+HaDeX_DT_format <- function(dat, cols = colnames(dat)) {
+  DT::datatable(
+    data = dat,
+    colnames = cols,
+    class = "table-bordered table-condensed",
+    extensions = "Buttons",
+    options = list(
+      pageLength = 10,
+      dom = "tBip",
+      autoWidth = TRUE,
+      buttons = c("excel", "pdf")
+    ),
+    filter = "bottom",
+    rownames = FALSE
+  )
+}
