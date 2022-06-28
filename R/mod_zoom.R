5347adce5e6fe2dc1d13dfa94ecbbb9ffad3a7a0
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-28 17:09:57 +0200

    change renderUIs to observers

--- R/mod_zoom.R
+++ R/mod_zoom.R
@@ -11,8 +11,22 @@ mod_zoom_ui <- function(id){
   ns <- NS(id)
   collapsible_card(
     title = "Zoom",
-    uiOutput(ns("gen_x_range")),
-    uiOutput(ns("gen_y_range")),
+    sliderInput(
+      inputId = ns("x_range"),
+      label = "Choose x range for butterfly plot:",
+      min = 0,
+      max = 1,
+      value = c(0, 1),
+      step = 1
+    ),
+    sliderInput(
+      inputId = ns("y_range"),
+      label = "Choose y range for butterfly plot:",
+      min = 0,
+      max = 1,
+      value = c(0, 1),
+      step = 1
+    ),
     init_collapsed = TRUE
   )
 }
@@ -24,21 +38,20 @@ mod_zoom_server <- function(id, dat_plot, fractional){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-    output[["gen_x_range"]] <- renderUI({
+    observe({
       max_x <- max(dat_plot()[["ID"]])
       min_x <- min(dat_plot()[["ID"]])
 
-      sliderInput(
-        inputId = ns("x_range"),
-        label = "Choose x range for butterfly plot:",
+      updateSliderInput(
+        session,
+        inputId = "x_range",
         min = min_x,
         max = max_x,
         value = c(min_x, max_x),
-        step = 1
       )
     })
 
-    output[["gen_y_range"]] <- renderUI({
+    observe({
       if (fractional()){
         max_y <- ceiling(max(dat_plot()[["frac_deut_uptake"]], dat_plot()[["theo_frac_deut_uptake"]], na.rm = TRUE)) + 1
         min_y <- floor(min(dat_plot()[["frac_deut_uptake"]], dat_plot()[["theo_frac_deut_uptake"]], na.rm = TRUE)) - 1
@@ -47,13 +60,12 @@ mod_zoom_server <- function(id, dat_plot, fractional){
         min_y <- floor(min(dat_plot()[["deut_uptake"]], dat_plot()[["theo_deut_uptake"]], na.rm = TRUE)) - 1
       }
 
-      sliderInput(
-        inputId = ns("y_range"),
-        label = "Choose y range for butterfly plot:",
+      updateSliderInput(
+        session,
+        inputId = "y_range",
         min = min_y,
         max = max_y,
         value = c(min_y, max_y),
-        step = 1
       )
     })
 
@@ -65,9 +77,3 @@ mod_zoom_server <- function(id, dat_plot, fractional){
     )
   })
 }
-
-## To be copied in the UI
-# mod_zoom_ui("zoom_1")
-
-## To be copied in the server
-# mod_zoom_server("zoom_1")
