f3383670518356d51868d89f9fce0ae38733d186
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-06-29 13:24:09 +0200

    include differential butterfly server in main server

--- R/app_server.R
+++ R/app_server.R
@@ -13,6 +13,19 @@ app_server <- function(input, output, session) {
 
   mod_uptake_butterfly_server(
     id = "butterfly",
+    differential = FALSE,
+    dat = dat_source[["dat"]],
+    chosen_protein = dat_source[["chosen_protein"]],
+    states_chosen_protein = dat_source[["states_chosen_protein"]],
+    times = dat_source[["times"]],
+    times_with_control = dat_source[["times_with_control"]],
+    deut_part = dat_source[["deut_part"]],
+    no_deut_control = dat_source[["no_deut_control"]]
+  )
+
+  mod_uptake_butterfly_server(
+    id = "butterfly_diff",
+    differential = TRUE,
     dat = dat_source[["dat"]],
     chosen_protein = dat_source[["chosen_protein"]],
     states_chosen_protein = dat_source[["states_chosen_protein"]],
