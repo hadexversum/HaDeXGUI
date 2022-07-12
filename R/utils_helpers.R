e501a83ad3c66b316dbeceefaf8dd7ca524fbf42
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-07-12 19:28:42 +0200

    restore loaders and add them to datatables
    closes #2

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -12,12 +12,6 @@ plotOutput_h <- function(outputId, ...)
          content = outputId, type = "markdown", buttonLabel = "Okay", easyClose = TRUE,
          icon = "far fa-question-circle", colour = "#856C9D")
 
-## "DT::dataTableOutput"
-dataTableOutput_h <- function(outputId, ...)
-  helper(getFromNamespace("dataTableOutput", ns = "DT")(outputId = outputId, ...), content = outputId,
-         type = "markdown", buttonLabel = "okay", easyClose = TRUE,
-         icon = "far fa-question-circle", colour = "#856C9D")
-
 func_vec <- c("selectInput", "textInput", "checkboxInput", "numericInput", "radioButtons", "checkboxGroupInput")
 func_list <- setNames(lapply(func_vec, function(ith_fun)
   tmp_name <- function(inputId, ...) {
@@ -30,8 +24,26 @@ for(ith_fun_id in 1L:length(func_list)) {
   assign(x = paste0(names(func_list)[ith_fun_id], "_h"), value = func_list[[ith_fun_id]])
 }
 
+with_hadex_spinner <- function(ui_element)
+  shinycustomloader::withLoader(
+    ui_element,
+    type = "image",
+    loader = "www/HaDeX_loader.gif"
+  )
+
+plotOutput_h <- function(outputId, ...)
+  helper(with_hadex_spinner(plotOutput(outputId = outputId, ...)),
+         content = outputId, type = "markdown", buttonLabel = "Okay", easyClose = TRUE,
+         icon = "far fa-question-circle", colour = "#856C9D")
+
+## "DT::dataTableOutput"
+dataTableOutput_h <- function(outputId, ...)
+  helper(with_hadex_spinner(getFromNamespace("dataTableOutput", ns = "DT")(outputId = outputId, ...)),
+         content = outputId, type = "markdown", buttonLabel = "Okay", easyClose = TRUE,
+         icon = "far fa-question-circle", colour = "#856C9D")
+
 ## "ggiraph::girafeOutput"
 girafeOutput_h <- function(outputId, ...)
-  helper(getFromNamespace("girafeOutput", ns = "ggiraph")(outputId = outputId, ...), content = outputId,
-         type = "markdown", buttonLabel = "okay", easyClose = TRUE,
+  helper(with_hadex_spinner(getFromNamespace("girafeOutput", ns = "ggiraph")(outputId = outputId, ...)),
+         content = outputId, type = "markdown", buttonLabel = "Okay", easyClose = TRUE,
          icon = "far fa-question-circle", colour = "#856C9D")
