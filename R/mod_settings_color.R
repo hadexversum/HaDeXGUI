a4f29fb232760262657cf724fa26f0ecc0faae86
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-07-13 13:27:02 +0200

    implement color settings logic

--- R/mod_settings_color.R
+++ R/mod_settings_color.R
@@ -11,21 +11,44 @@ mod_settings_color_ui <- function(id){
   ns <- NS(id)
   collapsible_card(
     title = "Colors",
+    uiOutput(ns("gen_state_color_pickers")),
+    init_collapsed = TRUE,
     fancy_icon = "palette"
   )
 }
+
+get_default_colors <- function(n)
+  hcl.colors(n, palette = "Set 2", alpha = NULL, rev = FALSE, fixup = TRUE)
+
 #' settings_color Server Functions
 #'
 #' @noRd
-mod_settings_color_server <- function(id){
+mod_settings_color_server <- function(id, s_state){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    output[["gen_state_color_pickers"]] <- renderUI({
+      states <- s_state %()% states
+      colors <- get_default_colors(length(states))
+
+      rlang::exec(
+        tagList,
+        !!!mapply(function(state, color) {
+          colourpicker::colourInput(
+            inputId = ns(paste0("color_", state)),
+            label = paste0("Color for ", state, " state"),
+            value = color)
+        }, states, colors, SIMPLIFY = FALSE)
+      )
+    })
+    outputOptions(output, "gen_state_color_pickers", suspendWhenHidden = FALSE)
+
+    return(
+      list(
+        values = reactive({
+          sapply(paste0("color_", s_state %()% states), function(x) input[[x]])
+        })
+      )
+    )
   })
 }
-
-## To be copied in the UI
-# mod_settings_color_ui("settings_color_1")
-
-## To be copied in the server
-# mod_settings_color_server("settings_color_1")
