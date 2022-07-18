29f415dbec74089b14b51e5155a3b841cec2f2dd
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-07-18 13:16:47 +0200

    fix mistakenly commenting out servers

--- R/app_server.R
+++ R/app_server.R
@@ -10,93 +10,93 @@ app_server <- function(input, output, session) {
 
   dat_source <- mod_data_load_server("data_load")
 
-  # mod_plot_comparison_and_woods_server(
-  #   id = "comparison_and_woods",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_volcano_server(
-  #   id = "volcano",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_butterfly_server(
-  #   id = "butterfly",
-  #   differential = FALSE,
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_butterfly_server(
-  #   id = "butterfly_diff",
-  #   differential = TRUE,
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_chiclet_server(
-  #   id = "chiclet",
-  #   differential = FALSE,
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_chiclet_server(
-  #   id = "chiclet_diff",
-  #   differential = TRUE,
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_uptake_server(
-  #   id = "uptake",
-  #   differential = FALSE,
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_uptake_server(
-  #   id = "uptake_diff",
-  #   differential = TRUE,
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # ### TIME-BASED DATA
-  #
-  # mod_plot_replicates_server(
-  #   id = "replicates",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_manhattan_server(
-  #   id = "manhattan",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_quality_control_server(
-  #   id = "quality_control",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # mod_plot_uncertainty_server(
-  #   id = "uncertainty",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
-  #
-  # ### MEASUREMENT
-  #
-  # mod_plot_measurements_server(
-  #   id = "measurements",
-  #   dat = dat_source[["dat"]],
-  #   params = dat_source[["params"]]
-  # )
+  mod_plot_comparison_and_woods_server(
+    id = "comparison_and_woods",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_volcano_server(
+    id = "volcano",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_butterfly_server(
+    id = "butterfly",
+    differential = FALSE,
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_butterfly_server(
+    id = "butterfly_diff",
+    differential = TRUE,
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_chiclet_server(
+    id = "chiclet",
+    differential = FALSE,
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_chiclet_server(
+    id = "chiclet_diff",
+    differential = TRUE,
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_uptake_server(
+    id = "uptake",
+    differential = FALSE,
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_uptake_server(
+    id = "uptake_diff",
+    differential = TRUE,
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  ### TIME-BASED DATA
+
+  mod_plot_replicates_server(
+    id = "replicates",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_manhattan_server(
+    id = "manhattan",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_quality_control_server(
+    id = "quality_control",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  mod_plot_uncertainty_server(
+    id = "uncertainty",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
+
+  ### MEASUREMENT
+
+  mod_plot_measurements_server(
+    id = "measurements",
+    dat = dat_source[["dat"]],
+    params = dat_source[["params"]]
+  )
 
   ### SEQUENCE DATA
 
