c2330e7edd02977ad2557389f5eb8889633f1cab
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-07-20 20:21:21 +0200

    Replace renders with observers
    closes #12

--- R/mod_data_hdexaminer.R
+++ R/mod_data_hdexaminer.R
@@ -16,18 +16,30 @@ mod_data_hdexaminer_ui <- function(id) {
       "For the additional information on how the data from HDeXaminer is processed, check the requirements above.",
       "Keep in mind that the MHP value is generated based on the peptide sequence and therefore, may differ from actual value in case of the modifications."
     ),
-    numericInput_h(inputId = ns("examiner_fd_timepoint"),
-                   label = "FD timepoint [min]:",
-                   value = 1440,
-                   min = 0,
-                   width = "100%"),
-    uiOutput(ns("gen_exam_protein_name")),
-    uiOutput(ns("gen_exam_state_name")),
-    checkboxGroupInput_h(inputId = ns("exam_confidence"),
-                         label = "Accepted confidence values:",
-                         choices = c("High", "Medium", "Low"),
-                         selected = c("Medium", "High")),
-    actionButton(inputId = ns("exam_apply_changes"),
+    numericInput_h(
+      inputId = ns("fd_timepoint"),
+      label = "FD timepoint [min]:",
+      value = 1440,
+      min = 0, #TODO: setup a validator?
+      width = "100%"
+    ),
+    textInput_h(
+      inputId = ns("protein_name"),
+      label = "Protein name:",
+      width = "100%"
+    ),
+    textInput_h(
+      inputId = ns("state_names"),
+      label = "State names:",
+      width = "100%"
+    ),
+    checkboxGroupInput_h(
+      inputId = ns("confidence"),
+      label = "Accepted confidence values:",
+      choices = c("High", "Medium", "Low"),
+      selected = c("Medium", "High")
+    ),
+    actionButton(inputId = ns("apply"),
                  label = "Apply changes to continue"),
     span(
       "The calculated values of MPH might slightly differ based on data used and its precision."
@@ -36,7 +48,7 @@ mod_data_hdexaminer_ui <- function(id) {
       href = "http://www.matrixscience.com/help/aa_help.html",
       "Used amino mass data"
     ),
-    DT::dataTableOutput(ns("checking_exam_data"))
+    DT::dataTableOutput(ns("updated_data"))
   )
 }
 
@@ -51,7 +63,7 @@ mod_data_hdexaminer_server <- function(id, dat_raw) {
 
     ### reactive values
 
-    proteins_from_file <- reactive({ ic(unique(dat_raw()[["Protein"]])) })
+    proteins_from_file <- reactive({ unique(dat_raw()[["Protein"]]) })
     states_from_file <- reactive({ unique(dat_raw()[["State"]]) })
 
     # this invalidation flag is to be sure that every time a new file is
@@ -60,7 +72,7 @@ mod_data_hdexaminer_server <- function(id, dat_raw) {
     observe({ invalidation_flag(0) }) %>%
       bindEvent(dat_raw())
     observe({ invalidation_flag(invalidation_flag() + 1) }) %>%
-      bindEvent(input[["exam_apply_changes"]])
+      bindEvent(input[["apply"]])
 
     dat_exam <- reactive({
       validate(need(invalidation_flag() > 0, "Apply changes in `Input Data` tab."))
@@ -68,34 +80,36 @@ mod_data_hdexaminer_server <- function(id, dat_raw) {
       # TODO: do something with the messages
       get_internal_messages(HaDeX::update_hdexaminer_file(
         dat = dat_raw(),
-        fd_time = input[["examiner_fd_timepoint"]],
+        fd_time = input[["fd_timepoint"]],
         old_protein_name = proteins_from_file(),
-        new_protein_name = input[["exam_protein_name"]],
+        new_protein_name = input[["protein_name"]],
         old_state_name = states_from_file(),
-        new_state_name = strsplit(input[["exam_state_name"]], ",")[[1]],
-        confidence = input[["exam_confidence"]]))
+        new_state_name = strsplit(input[["state_names"]], ",")[[1]],
+        confidence = input[["confidence"]]))
     }) %>% bindEvent(invalidation_flag())
 
 
-    ### ui outputs
+    ### observers updating ui
 
-    output[["gen_exam_protein_name"]] <- renderUI({
-      textInput_h(inputId = ns("exam_protein_name"),
-                  label = "Protein name:",
-                  value = proteins_from_file(),
-                  width = "100%")
+    observe({
+      updateTextInput(
+        session = session,
+        inputId = "protein_name",
+        value = proteins_from_file()
+      )
     })
 
-    output[["gen_exam_state_name"]] <- renderUI({
-      textInput_h(inputId = ns("exam_state_name"),
-                  label = "States names:",
-                  value = paste(states_from_file(), collapse = ", "),
-                  width = "100%")
+    observe({
+      updateTextInput(
+        session = session,
+        inputId = "state_names",
+        value = paste(states_from_file(), collapse = ", "),
+      )
     })
 
     ### other outputs
 
-    output[["checking_exam_data"]] <- DT::renderDataTable({
+    output[["updated_data"]] <- DT::renderDataTable({
       dat_exam() %>%
         select(Protein, State, Sequence,  Start, End, MHP) %>%
         unique(.) %>%
