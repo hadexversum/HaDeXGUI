b2594a375f100835f679e239a0e9389498c478b8
Weronika Puchała <ciemnamateria@gmail.com>
2025-02-13 15:17:32 +0100

    units on label y fixed

--- R/utils_text.R
+++ R/utils_text.R
@@ -33,8 +33,9 @@ idize <- function(x) gsub(" ", "_", tolower(x))
 react_construct_uptake_lab_y <- function(differential, env = parent.frame()) rlang::inject(
   reactive({
     frac_str <- if (s_calculation[["fractional"]]()) "Fractional " else ""
-    diff_str <- if (!!differential) "difference [%]" else if (s_calculation[["fractional"]]()) "[%]" else "[Da]"
-    capitalize(glue::glue("{frac_str}deuterium uptake {diff_str}"))
+    diff_str <- if (!!differential) "difference "
+    unit_str <- if (s_calculation[["fractional"]]()) "[%]" else "[Da]"
+    capitalize(glue::glue("{frac_str}deuterium uptake {diff_str} {unit_str}"))
   }, env = env)
 )
 
