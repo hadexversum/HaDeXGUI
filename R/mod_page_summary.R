880a0fc643c6cf09c2be55599c1cb1341470fbe7
Weronika Puchała <ciemnamateria@gmail.com>
2025-07-22 17:31:26 +0200

    summary for state or states

--- R/mod_page_summary.R
+++ R/mod_page_summary.R
@@ -12,6 +12,11 @@ mod_page_summary_ui <- function(id){
   hadex_tab_other(
     title = "Summary",
 
+    checkboxGroupInput_h(
+      inputId = ns("state"),
+      label = "Choose state:"
+    ),
+
     dataTableOutput_h(ns("table"), width = "60%"),
     includeMarkdown(app_sys("app/man/summary.md"))
   )
@@ -24,9 +29,22 @@ mod_page_summary_server <- function(id, dat, params){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
+    observe({
+      updateCheckboxGroupInput(
+        session,
+        inputId = "state",
+        choices = unique(dat()[["State"]]),
+        selected = unique(dat()[["State"]])
+      )
+    })
+
     dat_processed <- reactive({
+
+      validate(need(length(input[["state"]])>0, "Choose at least one state"))
+
+      dat() %>% filter(State %in% input[["state"]]) %>%
       HaDeX::show_summary_data(
-        dat = dat(),
+        dat = .,
         confidence_level = 0.9, #TODO: fix this hardcoded value!
         protein_length = params %()% max_range
       )
